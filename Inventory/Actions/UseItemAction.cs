68343266064410b6faa68cbdc60f86be6dc4fe5d
Christina Kyurkchieva <48100614+ChKyurkchieva@users.noreply.github.com>
2025-01-13 00:39:51 +0200

    Add inventory items

---
+++ Inventory/Actions/UseItemAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheUncodedOneContract.Interfaces;
+
+namespace Inventory.Actions;
+
+public class UseItemAction : IAction
+{
+	private readonly IItem _item;
+	private readonly IDisplay _display;
+
+	public UseItemAction(IItem item, IDisplay display)
+	{
+		_item = item;
+		_display = display;
+	}
+
+	public void Run(IBattle battle, ICharacter character)
+	{
+		_display.DisplayText($"{character.Name} was equiped with {_item.Name}");
+		IParty party = battle.GetEnemyPartyFor(battle.GetEnemyPartyFor(character).Characters[0]);
+		//party.Inventory.Remove(_item);
+		ItemData itemData = _item.Create();
+		character.HP += itemData.Points;
+	}
+}
---
+++ TheUncodedOneContract/Interfaces/IItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheUncodedOneContract.Interfaces;
+
+public record ItemData(int Points);
+public interface IItem
+{
+	string Name { get; }
+	ItemData Create();
+}
