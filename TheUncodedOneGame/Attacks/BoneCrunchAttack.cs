c9270435a1de92efb170ebef257e587f569846e4
Christina Kyurkchieva <48100614+ChKyurkchieva@users.noreply.github.com>
2025-01-13 15:14:26 +0200

    Removing duplicated project

--- TheUncodedOneGame/Attacks/BoneCrunchAttack.cs
+++
@@ -1,9 +0,0 @@
-﻿using TheUncodedOne.Contract.Interfaces;
-
-namespace TheUncodedOne.Game.Attacks;
-
-public class BoneCrunchAttack : IAttack
-{
-    public string Name => "BONE CRUNCH";
-    public AttackData Create() => new AttackData(new Random().Next(2));
-}
--- TheUncodedOneGame/Attacks/PunchAttack.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using TheUncodedOne.Contract.Interfaces;
-namespace TheUncodedOne.Game.Attacks;
-
-public class PunchAttack : IAttack
-{
-    public string Name => "PUNCH";
-    public AttackData Create() => new AttackData(1);
-}
